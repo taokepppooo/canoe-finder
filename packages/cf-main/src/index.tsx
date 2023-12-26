52a70effab82d58adb58fd1d13648d5d64f9f12e
taokepppooo <1075626521@qq.com>
2023-12-26 11:19:18 +0800

    chore: 添加eslint prettier

--- packages/cf-main/src/index.tsx
+++ packages/cf-main/src/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Main } from './main';
-import './index.css'
-
+import './index.css';
 
 const container = document.querySelector('#root');
 const root = createRoot(container);
--- packages/cf-main/src/main.tsx
+++ packages/cf-main/src/main.tsx
@@ -1,28 +1,24 @@
-import React, { useState } from "react";
-import "./main.css";
-import reactLogo from "./assets/logo.svg";
+import React, { useState } from 'react';
+import './main.css';
+import reactLogo from './assets/logo.svg';
 export function Main() {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Farm + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
           Edit <code>src/main.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Farm and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Farm and React logos to learn more</p>
     </>
   );
 }
